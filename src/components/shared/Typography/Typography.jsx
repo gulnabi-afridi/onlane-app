b1760906715a8e769d1c84dede2c24da520dcaa8
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-12-23 00:29:04 +0500

    custom typography added

--- src/components/shared/Typography/Typography.jsx
+++ src/components/shared/Typography/Typography.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-export const MediumText = ({ text }) => {
-  return <p className='text-[15px] font-normal '></p>;
+export const MediumText = ({ children, styles = 'text-black-main' }) => {
+  return (
+    <p className={`text-[15px] font-normal font-poppins ${styles}`}>
+      {children}
+    </p>
+  );
 };
 
-export const LightText = ({ text }) => {
-  return <p className='text-[15px] font-extralight '></p>;
+export const LightText = ({ children, styles = 'text-black-main' }) => {
+  return <p className={`text-[15px] font-extralight ${styles}`}>{children}</p>;
 };
 
-export const NormalText = ({ text }) => {
-  return <p className='text-[15px] font-light'></p>;
+export const NormalText = ({ children, styles = 'text-black-main' }) => {
+  return <p className={`text-[15px] font-light ${styles}`}>{children}</p>;
 };
 
-export const Heading1 = ({ text }) => {
-  return <p className='text-[35px] font-extrabold'></p>;
+export const Heading1 = ({ children, styles = 'text-black-main' }) => {
+  return <p className={`text-[35px] font-extrabold ${styles}`}>{children}</p>;
 };
 
-export const Heading2 = ({ text }) => {
-  return <p className='text-[25px] font-semibold'></p>;
+export const Heading2 = ({ children, styles = 'text-black-main' }) => {
+  return <p className={`text-[25px] font-semibold ${styles}`}>{children}</p>;
+};
+
+export const Heading3 = ({ children, styles = 'text-black-main' }) => {
+  return <p className={`text-[20px] font-semibold ${styles}`}>{children}</p>;
 };
 
 const Typography = {
@@ -26,6 +34,7 @@ const Typography = {
   NormalText,
   H1: Heading1,
   H2: Heading2,
+  H3: Heading3,
 };
 
 export default Typography;
