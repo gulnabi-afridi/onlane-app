355060cb85f78e756280c4a5ce20622b5e7bbef1
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-12 20:13:31 +0500

    height changes

--- src/components/dashboard/Dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import Widget from './Widget';
 
 const Dashboard = () => {
   return (
-    <div className='w-full min-h-screen flex flex-col px-2 md:px-8 xl:px-14'>
+    <div className='w-full h-auto sm:min-h-screen flex flex-col px-2 md:px-8 xl:px-14'>
       <div className='w-full h-[140px] flex flex-col justify-center items-start gap-2'>
         <Typography.H1 styles='text-black-main font-extrabold'>
           Dashboard
@@ -27,7 +27,7 @@ const Dashboard = () => {
         })}
       </div>
       {/* upcomming payment ---->  */}
-      <div className='w-full min-h-[110px] flex flex-col gap-3 my-4 p-4 sm:p-5 bg-white-main rounded-[15px] uiShadow1 border-[1px] border-black-main/10'>
+      <div className='w-full h-auto sm:min-h-[110px] flex flex-col gap-3 my-4 p-4 sm:p-5 bg-white-main rounded-[15px] uiShadow1 border-[1px] border-black-main/10'>
         <div className='w-full flex justify-between items-center'>
           <Typography.H2 styles='text-black-main font-poppins font-semibold '>
             Upcoming Payment
@@ -70,7 +70,7 @@ const Dashboard = () => {
             {chargingSessionTable.rows.map((item, rowIndex) => (
               <div
                 key={rowIndex}
-                className='w-full min-h-[35px] grid grid-cols-[1fr,1fr,1fr,1fr,1.2fr,1fr] px-4 md:px-10'
+                className='w-full h-auto sm:min-h-[35px] grid grid-cols-[1fr,1fr,1fr,1fr,1.2fr,1fr] px-4 md:px-10'
               >
                 {item.row.map((rowItem, colIndex) => (
                   <div
